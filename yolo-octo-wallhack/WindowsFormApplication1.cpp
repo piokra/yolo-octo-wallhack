d7d0f682480fd547ad4ddc712162b18326d76c3c
Pan Piotr <piotrkrasun@gmail.com>
2015-05-31 22:23:44 +0200

    Actually added examples, moved conversion.h

--- yolo-octo-wallhack/WindowsFormApplication1.cpp
+++ yolo-octo-wallhack/WindowsFormApplication1.cpp
@@ -12,109 +12,16 @@
 #include "piotr\math\multiplictionoffunctions.h"
 using namespace yolo_octo_wallhack;
 
-namespace Piotr
-{
-	namespace Math
-	{
-		/*
-		void SITestFunction()
-		{
-			StringInterpreter si;
-			si.pushLine("Real a");
-			si.pushLine("Real t");
-			si.pushLine("Real b");
-			si.pushLine("Real c");
-			si.pushLine("Real d");
-			si.run();
-			Output^ op = gcnew Output(si.seperateWords("f(2+3*4*55+swag13)"," ","()*+-/^"));
-			op->Show();
-			op = gcnew Output(si.seperateWords(("(2+3)*5"), " ", "()+-*^,"));
-			op->Show();
-			op = gcnew Output(si.seperateWords(("((a+t)*((b+(a+c))^(c+d)))"), " ", "()+-*^/,"));
-			op->Show();
-			op = gcnew Output(si.toRPN("((a+t)*((b+(a+c))^(c+d)))"));
-			op->Show();
-
-		}*/
-		void SITestFunction2()
-		{
-			/*
-			StringInterpreter si;
-			Form^ form = gcnew Output(si.removeSpaces("Hello my name is yoloswag13."));
-			form->Show();
-			form = gcnew Output(si.seperateWords("Hello my name      is yoloswag13.", " ", ""));
-			form->Show();
-			si.compileLine("");
-			si.compileLine("YOLO SWAG 13");
-			si.compileLine("Real yoloswag13");
-			si.compileLine("DisplayReal yoloswag13");
-			si.showResult();
-			si.showLog();
-			*/
-		}
-		class THC
-		{
-		public:
-			ManagedArgument m;
-			GenericMathFunction* get()
-			{
-				return (GenericMathFunction*)m.get();
-			}
-			Variable* v()
-			{
-				return (Variable*)m.get();
-			}
-			Constant* c()
-			{
-				return (Constant*)m.get();
-			}
-			GenericMathFunction* operator->()
-			{
-				return get();
-			}
-		};
-		void FunctionTestFunction()
-		{
-			ManagedArgument mc(new Constant(1));
-			THC tmc{ mc };
-			std::string str;
-			tmc.c()->set(ManagedArgument(new Real(7)));
-			THC tmc2;
-			tmc2.m = tmc->operator+(tmc.m);
-			tmc.get()->operator()(ManagedArgument())->toString(str);
-			Output^ out = gcnew Output(str);
-			out->Show();
-			str = "";
-			tmc2.get()->operator()(ManagedArgument())->toString(str);
-			out = gcnew Output(str);
-			out->Show();
-			THC tmc3 = { ManagedArgument(new Variable(0, 0)) };
-			tmc2.m = tmc->operator*(tmc3.m);
-			str = "";
-			tmc2.get()->operator()(ManagedArgument(new Real(7)))->toString(str);
-			out = gcnew Output(str);
-			out->Show();
-
-		}
-	}
-}
 
 
 [STAThreadAttribute]
 int main(array<System::String ^> ^args)
 {
 	
-	StringInterpreter si;
-	// Enabling Windows XP visual effects before any controls are created
+
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false); 
-	//FunctionTestFunction();
-	//SITestFunction();
-	// Create the main window and run it
-	//SITestFunction();
-	ManagedArgument ma = ManagedArgument(new Variable(1, 0));
-	Graph^ graph = gcnew Graph(ma, 0, 1, 0.1);
-	graph->Show();
+
 	Application::Run(gcnew Input());
 
 	return 0;
